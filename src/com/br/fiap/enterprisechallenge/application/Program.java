985d9a1d56cda90a0588475d1a42a57d7c11f469
Amauri Junior <apojr98@gmail.com>
2022-05-30 21:25:09 -0300

    BackEnd Completed

--- src/com/br/fiap/enterprisechallenge/application/Program.java
+++ src/com/br/fiap/enterprisechallenge/application/Program.java
@@ -17,12 +17,12 @@ public class Program {
 		PessoaDao pessoaDao = DaoFactory.createPessoaDao();
 		
 		System.out.println("==== TEST 1: pessoa findByID ====");
-		Pessoa pessoa = pessoaDao.findById(3);
+		Pessoa pessoa = pessoaDao.findById(5);
 		
 		System.out.println(pessoa);
-		
+			
 		System.out.println("\n==== TEST 2: pessoa findByDoenca ====");
-		Doenca doenca = new Doenca(2, null);
+		Doenca doenca = new Doenca(3, null);
 		List<Pessoa> list = pessoaDao.findByDoenca(doenca);
 		for(Pessoa obj : list) {
 			System.out.println(obj);
@@ -38,11 +38,11 @@ public class Program {
 		pessoaDao.insert(newPessoa);
 		System.out.println("Inserted! New id = " + newPessoa.getId());
 		
-		System.out.println("\n==== TEST 5: pessoa update ====");
-		pessoa = pessoaDao.findById(1);
-		pessoa.setRegiao("Norte");
-		pessoaDao.update(pessoa);
-		System.out.println("Update completed");
+//		System.out.println("\n==== TEST 5: pessoa update ====");
+//		pessoa = pessoaDao.findById(1);
+//		pessoa.setRegiao("Norte");
+//		pessoaDao.update(pessoa);
+//		System.out.println("Update completed");
 		
 		System.out.println("\n==== TEST 6: pessoa delete ====");
 		System.out.println("Enter id for delete test: ");
